40c127623ff79aba3264b8195795e2a9f79edb54
v010 <v010@benner.com.br>
2018-07-11 15:58:28 -0300

    Exercicio mediaIdades adicionado

---
+++ T0/C# 1/MediaIdades/MediaIdades/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaIdades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int maria = 18, joao = 20, gabriel = 10;
+            double media = (maria+joao+gabriel) / 3;
+            MessageBox.Show("A media de idades é: "+media);
+        }
+    }
+}
