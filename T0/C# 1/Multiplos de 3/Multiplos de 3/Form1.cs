767a55c44470c758173d2fe416f29cbc3c134a74
v010 <v010@benner.com.br>
2018-07-12 16:15:31 -0300

    Exercicio multiplos de 3 Adicionado

---
+++ T0/C# 1/Multiplos de 3/Multiplos de 3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplos_de_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i <= 100; i++)
+            {
+                if(i % 3 == 0 && i!=0)
+                {
+                    MessageBox.Show(" O numero, " + i + " é multiplo de três");
+                }
+            }
+        }
+    }
+}
