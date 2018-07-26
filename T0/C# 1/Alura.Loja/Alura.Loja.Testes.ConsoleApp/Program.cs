8980a3b17a0087055dab4816ef66db9f9d7af5dc
v010 <v010@benner.com.br>
2018-07-26 14:46:14 -0300

    Adicionando class compra e fazendo update Data-base

--- T0/C# 1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T0/C# 1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,81 +10,18 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoEntity();
-            RecuperarProdutos();
-            // RemoverProdutos();
-            //RecuperarProdutos();
-            //AtualizarProdutos();
-        }
+            // Compra de 6 pães 
+            var paoFrances = new Produto();
+            paoFrances.Nome = " Pão Francês";
+            paoFrances.PrecoUnitario = 0.40;
+            //paofrances.unidade = 1;
+            paoFrances.Categoria = "Padaria";
 
-        private static void AtualizarProdutos()
-        {
-            // incluir um novo produto
-            GravarUsandoEntity();
-            RecuperarProdutos();
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
 
-            // atualizar produto
-            using (var repo = new ProdutoDAOEntity())
-            {
-                Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Texto Editado";
-                repo.Atualizar(primeiro);
-
-            }
-
-            RecuperarProdutos();
-
-        }
-
-        private static void RemoverProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach (var item in produtos)
-                {
-                    repo.Remover(item);
-                }
-
-            }
-        }
-
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
-                foreach (var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
-            }
-        }
-
-        private static void GravarUsandoEntity()
-        {
-            Produto p1 = new Produto();
-            p1.Nome = "Harry Potter e a Ordem da Fênix";
-            p1.Categoria = "Livros";
-            p1.PrecoUnitario = 19.89;
-
-            Produto p2 = new Produto();
-            p2.Nome = "Senhor dos Anéis 1";
-            p2.Categoria = "Livros";
-            p2.PrecoUnitario = 19.89;
-
-            Produto p3 = new Produto();
-            p3.Nome = "O Monge e o Executivo";
-            p3.Categoria = "Livros";
-            p3.PrecoUnitario = 19.89;
-
-            using (var repo = new ProdutoDAOEntity())
-            {
-                repo.Adicionar(p1);
-                repo.Adicionar(p2);
-                repo.Adicionar(p3);
-            }
         }
     }
 }
