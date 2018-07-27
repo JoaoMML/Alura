63a9682c9e789a7b142e6c0bd4e1cfd0005e4998
v010 <v010@benner.com.br>
2018-07-27 16:03:16 -0300

    Adicionando cliente e endereço relaçao

--- T0/C# 1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T0/C# 1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,38 +10,62 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
+        public static string Logradouro { get; private set; }
+        public static string Complemento { get; private set; }
+        public static string Bairro { get; private set; }
+        public static string Cidade { get; private set; }
+
         static void Main(string[] args)
         {
-            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
-            var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
-            var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
-
-            var promocaoDePascoa = new Promocao();
-            promocaoDePascoa.Descricao = "Pascoa Feliz";
-            promocaoDePascoa.DataInicio = DateTime.Now;
-            promocaoDePascoa.DataFinal = DateTime.Now.AddMonths(3);
-
-            promocaoDePascoa.IncluiProduto(p1);
-            promocaoDePascoa.IncluiProduto(p2);
-            promocaoDePascoa.IncluiProduto(p3);
-
+            var fulano = new Cliente();
+            fulano.Nome = " Fulaninho de tal ";
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = 18,
+                Logradouro = "Rua Yolanda Grossi Cabral",
+                Complemento = "Casa com sobrado ",
+                Bairro = "Centro",
+                Cidade = "Registro",
+            };
 
             using (var contexto = new LojaContext())
             {
-                //contexto.Promocoes.Add(promocaoDePascoa);
-                var promocao = contexto.Promocoes.Find(3);
-                contexto.Promocoes.Remove(promocao);
+                contexto.Clientes.Add(fulano);
                 contexto.SaveChanges();
             }
-        }      
+    }
+
+    private static void MuitosParaMuitos()
+    {
+        var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
+        var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
+        var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
+
+        var promocaoDePascoa = new Promocao();
+        promocaoDePascoa.Descricao = "Pascoa Feliz";
+        promocaoDePascoa.DataInicio = DateTime.Now;
+        promocaoDePascoa.DataFinal = DateTime.Now.AddMonths(3);
+
+        promocaoDePascoa.IncluiProduto(p1);
+        promocaoDePascoa.IncluiProduto(p2);
+        promocaoDePascoa.IncluiProduto(p3);
 
 
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        using (var contexto = new LojaContext())
         {
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
+            //contexto.Promocoes.Add(promocaoDePascoa);
+            var promocao = contexto.Promocoes.Find(1);
+            contexto.Promocoes.Remove(promocao);
+            contexto.SaveChanges();
         }
     }
+
+    private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+    {
+        foreach (var e in entries)
+        {
+            Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+        }
+    }
+}
 }
