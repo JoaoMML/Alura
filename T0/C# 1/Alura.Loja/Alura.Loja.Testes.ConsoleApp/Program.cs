a2674678cfcdbc62bab359c72bb0f3f2d99b6514
v010 <v010@benner.com.br>
2018-07-27 16:31:21 -0300

    Adicionando select com join em Relação M -> N

--- T0/C# 1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T0/C# 1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,51 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
-        public static string Logradouro { get; private set; }
-        public static string Complemento { get; private set; }
-        public static string Bairro { get; private set; }
-        public static string Cidade { get; private set; }
-
         static void Main(string[] args)
+        {
+            using (var contexto2 = new LojaContext())
+            {
+                var promocao = contexto2
+                    .Promocoes
+                    .Include(p=> p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+                Console.WriteLine("\n Motrando os produtos da promoção...");
+                foreach (var item in promocao.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
+            }
+
+
+        }
+
+        private static void IncluirPromocao()
+        {
+            using (var contexto = new LojaContext())
+            {
+                var promocao = new Promocao();
+                promocao.Descricao = "Queima de estoque Jan/2019";
+                promocao.DataInicio = new DateTime(2019, 1, 1);
+                promocao.DataFinal = new DateTime(2019, 1, 31);
+
+
+                var produtos = contexto
+                    .Produtos
+                    .Where(p => p.Categoria == "Bebidas")
+                    .ToList();
+
+                foreach (var item in produtos)
+                {
+                    promocao.IncluiProduto(item);
+                }
+
+                contexto.Promocoes.Add(promocao);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void UmParaUm()
         {
             var fulano = new Cliente();
             fulano.Nome = " Fulaninho de tal ";
@@ -33,39 +73,39 @@ namespace Alura.Loja.Testes.ConsoleApp
                 contexto.Clientes.Add(fulano);
                 contexto.SaveChanges();
             }
-    }
+        }
 
-    private static void MuitosParaMuitos()
-    {
-        var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
-        var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
-        var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
-
-        var promocaoDePascoa = new Promocao();
-        promocaoDePascoa.Descricao = "Pascoa Feliz";
-        promocaoDePascoa.DataInicio = DateTime.Now;
-        promocaoDePascoa.DataFinal = DateTime.Now.AddMonths(3);
-
-        promocaoDePascoa.IncluiProduto(p1);
-        promocaoDePascoa.IncluiProduto(p2);
-        promocaoDePascoa.IncluiProduto(p3);
-
-
-        using (var contexto = new LojaContext())
+        private static void MuitosParaMuitos()
         {
-            //contexto.Promocoes.Add(promocaoDePascoa);
-            var promocao = contexto.Promocoes.Find(1);
-            contexto.Promocoes.Remove(promocao);
-            contexto.SaveChanges();
+            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
+            var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
+            var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
+
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Pascoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataFinal = DateTime.Now.AddMonths(3);
+
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
+
+
+            using (var contexto = new LojaContext())
+            {
+                //contexto.Promocoes.Add(promocaoDePascoa);
+                var promocao = contexto.Promocoes.Find(1);
+                contexto.Promocoes.Remove(promocao);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
         }
     }
-
-    private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-    {
-        foreach (var e in entries)
-        {
-            Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-        }
-    }
-}
 }
