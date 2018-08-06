0e30e53f17b5ea99c041f71367679d1888407fc0
v010 <v010@benner.com.br>
2018-08-06 15:11:37 -0300

    Adicionando arquivo inicial MVC

---
+++ T0/ASP.NET - MVC/Inicial/Inicial/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inicial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // codigo da regra de negócio da aplicação
+            // View mostra para o usuário
+            return View();
+        }
+    }
+}
