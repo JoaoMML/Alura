87623d9f22615c57dd6289629a3cfcc2ac5101d9
v010 <v010@benner.com.br>
2018-08-07 17:22:14 -0300

    Trabalhando melhor com as views

--- T0/ASP.NET - MVC/CaelumEstoque/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ T0/ASP.NET - MVC/CaelumEstoque/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -14,24 +14,46 @@ namespace CaelumEstoque.Controllers
         public ActionResult Index()
         {
             ProdutosDAO dao = new ProdutosDAO();
-            IList<Produto> produtos = dao.Lista();
-            ViewBag.Produtos = produtos;
+            var produtos = dao.Lista();
+            return View(produtos);
+        }
+        public ActionResult Form()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            ViewBag.Produto = new Produto();
+            ViewBag.Categorias = dao.Lista();
             return View();
         }
 
-        public ActionResult Form()
-        {
-            CategoriasDAO categoriasDAO = new CategoriasDAO();
-            IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
-            ViewBag.Categorias = categorias;
-            return View();
-        }
+
         [HttpPost]
         public ActionResult Adiciona(Produto produto)
         {
+            int idDaInformatica = 1;
+            if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.InformaticaComPrecoInvalido", "Produtos da categoria informática devem ter preço maior do que 100");
+            }
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Form");
+            }
+        }
+        public ActionResult Visualiza(int id)
+        {
             ProdutosDAO dao = new ProdutosDAO();
-            dao.Adiciona(produto);
-            return RedirectToAction("Index", "Produto");
+            Produto produto = dao.BuscaPorId(id);
+            ViewBag.Produto = produto;
+            return View();
         }
     }
 }
