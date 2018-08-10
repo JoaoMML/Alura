1f58a4304443053b8d75c04f70f0f5d454dc8bc6
v010 <v010@benner.com.br>
2018-08-10 15:44:07 -0300

    Filtro para autorização de usuários e restringindo o acesso

---
+++ T0/ASP.NET - MVC/Inicial/Inicial/Teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace Inicial
+{
+    public class Teste
+    {
+    }
+}
