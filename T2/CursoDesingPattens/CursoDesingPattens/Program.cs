29bb3c38e29fbb473b0dac7085cdab7fe8d3bba8
v010 <v010@benner.com.br>
2018-08-14 17:08:01 -0300

    Adicionando a class DescontoPorVendaCasada e implementando a ordem de verificação de descontos

--- T2/CursoDesingPattens/CursoDesingPattens/Program.cs
+++ T2/CursoDesingPattens/CursoDesingPattens/Program.cs
@@ -14,16 +14,17 @@ namespace CursoDesingPattens
 
             Orcamento orcamento = new Orcamento(500);
             orcamento.AdicionaItem(new Item("CANETA", 250));
-            orcamento.AdicionaItem(new Item("LAPIS", 250));
-            orcamento.AdicionaItem(new Item("XBOX", 250));
-            orcamento.AdicionaItem(new Item("PAPEL", 250));
-            orcamento.AdicionaItem(new Item("PC", 250));
-            orcamento.AdicionaItem(new Item("GELADEIRA", 250));
-            orcamento.AdicionaItem(new Item("CANECA", 250));
+            //orcamento.AdicionaItem(new Item("LAPIS", 250));
+            //orcamento.AdicionaItem(new Item("XBOX", 250));
+            //orcamento.AdicionaItem(new Item("PAPEL", 250));
+            //orcamento.AdicionaItem(new Item("PC", 250));
+            //orcamento.AdicionaItem(new Item("GELADEIRA", 250));
+            //orcamento.AdicionaItem(new Item("CANECA", 250));
 
             double desconto = calculador.Calcula(orcamento);
             Console.WriteLine(desconto);
             Console.ReadKey();
+
         }
     }
 }
