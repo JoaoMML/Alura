143836304ca5ddac7073d706b5825eaf95c289a9
v010 <v010@benner.com.br>
2018-08-21 16:36:01 -0300

    Fazendo uma interface fluente

--- T2/CursoDesingPattens/CursoDesingPattens/Program.cs
+++ T2/CursoDesingPattens/CursoDesingPattens/Program.cs
@@ -11,13 +11,13 @@ namespace CursoDesingPattens
         static void Main(string[] args)
         {
             CriadorDeNotaFiscal criador = new CriadorDeNotaFiscal();
-            criador.ParaEmpresa("Caelum ensino e Inovacao");
-            criador.ComCnpj("23.456.789/0001-12");
-            //...
-            criador.ComItem(new ItemDaNota("item 1",100.0 ));
-            criador.ComItem(new ItemDaNota("item 2", 200.0));
-            criador.NaDataAtual();
-            criador.ComObservacoes("uma obs qualquer");
+            criador
+                .ParaEmpresa("Caelum ensino e Inovacao")
+                .ComCnpj("23.456.789/0001-12")
+                .ComItem(new ItemDaNota("item 1",100.0 ))
+                .ComItem(new ItemDaNota("item 2", 200.0))
+                .NaDataAtual()
+                .ComObservacoes("uma obs qualquer");
 
             NotaFiscal nf = criador.Controi();
             Console.WriteLine(nf.ValorBruto);
