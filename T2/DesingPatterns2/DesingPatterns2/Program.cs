40d1f3419e01a5599330c185f7232babd1f9f986
v010 <v010@benner.com.br>
2018-08-22 14:58:49 -0300

    Iniciando curso Desing Patterns2

---
+++ T2/DesingPatterns2/DesingPatterns2/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new SqlConnection(); 
+        }
+    }
+}
