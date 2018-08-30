613369d9fc3e2b34263129023809158c93a31b4b
v010 <v010@benner.com.br>
2018-08-30 15:12:51 -0300

    Finalizando cap 2 Solid

--- T2/Solid/Solid/Program.cs
+++ T2/Solid/Solid/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Solid.Cap_2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace Solid
     {
         static void Main(string[] args)
         {
+            EnviadorDeEmail enviadorEmail = new EnviadorDeEmail();
+            NotaFiscalDao nfDao = new NotaFiscalDao();
+            IList<IAcaoAposGerarNota> acoes = new List<IAcaoAposGerarNota>();
+            acoes.Add(new EnviadorDeEmail());
+            acoes.Add(new NotaFiscalDao());
+            acoes.Add(new SAP());
+            GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(acoes);
+            Fatura fatura = new Fatura(200, "Joao Marcos");
+            gerador.Gera(fatura);
+            Console.ReadKey();
         }
     }
 }
