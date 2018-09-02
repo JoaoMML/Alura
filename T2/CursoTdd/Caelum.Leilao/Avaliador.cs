7b3f6e7762848ea5330df70aa730f50a4970929a
Joao Marcos de Moraes Lima <joaomarcos_rgt@hotmail.com>
2018-09-02 19:47:50 -0300

    Finalizando o curso de solid e começando TDD

---
+++ T2/CursoTdd/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Caelum.Leilao
+{
+    public class Avaliador
+    {
+        private double maiorDeTodos = Double.MinValue;
+        private double menorDeTodos = Double.MaxValue;
+
+        public void Avalia(Leilao leilao)
+        {
+            foreach(var lance in leilao.Lances)
+            {
+                if(lance.Valor> maiorDeTodos)
+                {
+                    maiorDeTodos = lance.Valor;
+                }
+                else if(lance.Valor > menorDeTodos)
+                {
+                    menorDeTodos = lance.Valor;
+                }
+            }
+        }
+    }
+}
--- T2/Solid/Solid/Program.cs
+++ T2/Solid/Solid/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Solid.Cap_2;
 using Solid.Cap_3;
 using Solid.Cap_4;
+using Solid.Cap5;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,27 @@ namespace Solid
     {
         static void Main(string[] args)
         {
+            IList<ContaComum> contas = ContasDoBanco();
+
+            foreach(ContaComum conta in contas)
+            {
+                conta.Deposita(120);
+                conta.CalculaValorDoJurosMensal();
+                conta.SomaInvestimento();
+
+                Console.WriteLine("Novo saldo: "+ conta.Saldo);
+            }
+            Console.ReadLine();
         }
+
+        private static IList<ContaComum> ContasDoBanco()
+        {
+            List<ContaComum> contas = new List<ContaComum>();
+            contas.Add(umaContaComum(100));
+            contas.Add(umaContaComum(150));
+            contas.Add(umaContaComum(100));
+            return contas;
+        }
+
     }
 }
