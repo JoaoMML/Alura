dcc1a53e597ce3fc353011683a6aa31b33175f29
v010 <v010@benner.com.br>
2018-09-04 17:40:18 -0300

    Quase finalizando o cap3 do Tdd

--- T2/CursoTdd/Caelum.Leilao/Leilao.cs
+++ T2/CursoTdd/Caelum.Leilao/Leilao.cs
@@ -16,8 +16,47 @@ namespace Caelum.Leilao
 
         public void Propoe(Lance lance)
         {
-            Lances.Add(lance);
+            if (Lances.Count == 0 || (
+                !ultimoLanceDado().Usuario.Equals(lance.Usuario) &&
+                qtdDelancesDo(lance.Usuario) < 5))
+            {
+                Lances.Add(lance);
+            }
         }
 
+        private int qtdDelancesDo(Usuario usuario)
+        {
+            int total = 0;
+            foreach (Lance lance in Lances)
+            {
+                if (lance.Usuario.Equals(usuario)) total++;
+            }
+            return total;
+        }
+
+        private Lance ultimoLanceDado()
+        {
+            return Lances[Lances.Count - 1];
+        }
+
+        public void DobraLance(Usuario usuario)
+        {
+            Lance ultimoLance = ultimoLanceDo(usuario);
+            if (ultimoLance != null)
+            {
+                Propoe(new Lance(usuario, ultimoLance.Valor * 2));
+            }
+        }
+
+        private Lance ultimoLanceDo(Usuario usuario)
+        {
+            Lance ultimo = null;
+            foreach (Lance lance in Lances)
+            {
+                if (lance.Usuario.Equals(usuario)) ultimo = lance;
+            }
+
+            return ultimo;
+        }
     }
 }
--- T2/CursoTdd/Caelum.Leilao/Usuario.cs
+++ T2/CursoTdd/Caelum.Leilao/Usuario.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Caelum.Leilao
 {
 
@@ -15,6 +17,26 @@ namespace Caelum.Leilao
         {
             this.Id = id;
             this.Nome = nome;
+        }
+        public override bool Equals(Object obj)
+        {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (GetType() != obj.GetType())
+                return false;
+            Usuario other = (Usuario)obj;
+            if (Id != other.Id)
+                return false;
+            if (Nome == null)
+            {
+                if (other.Nome != null)
+                    return false;
+            }
+            else if (!Nome.Equals(other.Nome))
+                return false;
+            return true;
         }
 
     }
