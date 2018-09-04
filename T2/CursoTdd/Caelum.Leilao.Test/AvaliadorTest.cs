a1617cf78aa8384fba754fc22ff0c3fbf29c7204
v010 <v010@benner.com.br>
2018-09-04 16:35:14 -0300

    Finalizando cap 2 do curso de TDD

--- T2/CursoTdd/Caelum.Leilao.Test/AvaliadorTest.cs
+++ T2/CursoTdd/Caelum.Leilao.Test/AvaliadorTest.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Caelum.Leilao.Test;
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Caelum.Leilao
 {
@@ -55,17 +57,168 @@ namespace Caelum.Leilao
         public void DeveEntenderLeilaoComApenasUmLance()
         {
             Usuario joao = new Usuario("Joao");
-            Leilao leilao = new Leilao("Playstation 3 Novo");
+            Leilao leilao = new Leilao("Xbox Novo");
 
-            leilao.Propoe(new Lance(joao, 1000.0));
+            leilao.Propoe(new Lance(joao, 2000.0));
 
             Avaliador leiloeiro = new Avaliador();
             leiloeiro.Avalia(leilao);
 
-            Assert.AreEqual(1000, leiloeiro.MaiorLance, 0.0001);
-            Assert.AreEqual(1000, leiloeiro.MenorLance, 0.0001);
+            Assert.AreEqual(2000, leiloeiro.MaiorLance, 0.0001);
+            Assert.AreEqual(2000, leiloeiro.MenorLance, 0.0001);
         }
 
+        [Test]
+        public void DeveEntenderOsTresMaioresLances()
+        {
+            Usuario joao = new Usuario("joao");
+            Usuario maria = new Usuario("maria");
+            Leilao leilao = new Leilao("Xbox Novo");
+
+            leilao.Propoe(new Lance(joao, 100.0));
+            leilao.Propoe(new Lance(maria, 200.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(maria, 400.0));
+
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            var maiores = leiloeiro.TresMaiores;
+            Assert.AreEqual(3, maiores.Count);
+            Assert.AreEqual(400, maiores[0].Valor, 0.0001);
+            Assert.AreEqual(300, maiores[1].Valor, 0.00001);
+            Assert.AreEqual(200, maiores[2].Valor, 0.00001);
+        }
+        [Test]
+        public void DeveEntenderLancesAleatorios() { 
+        Usuario joao = new Usuario("joao");
+        Usuario maria = new Usuario("maria");
+        Leilao leilao = new Leilao("Xbox Novo");
+
+        leilao.Propoe( new Lance(joao,100.0));
+            leilao.Propoe(new Lance(maria, 200.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(maria, 400.0));
+            leilao.Propoe(new Lance(joao, 700.0));
+            leilao.Propoe(new Lance(maria, 550.0));
+            leilao.Propoe(new Lance(joao, 120.0));
+            leilao.Propoe(new Lance(maria, 50.0));
+
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            Assert.AreEqual(50, leiloeiro.MenorLance,0.0001);
+            Assert.AreEqual(700, leiloeiro.MaiorLance,0.0001);
+            
+        }
+
+        [Test] // Adc
+        public void TesteOrdemDecrescente() // Deixar publico e sem nada dentro do Main()
+        {   // Cenário - 1° parte do teste
+            Usuario joao = new Usuario("Joao");
+            Usuario jose = new Usuario("José");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(joao, 900.0));
+            leilao.Propoe(new Lance(jose, 500.0));
+            leilao.Propoe(new Lance(maria, 50.0));
+
+            // Ação - 2° parte
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            // Validação - 3° parte
+            Assert.AreEqual(900, leiloeiro.MaiorLance,0.00001); // imprime 400.0
+            Assert.AreEqual(50, leiloeiro.MenorLance,0.0001); // imprime 200.0
+        }
+
+        [Test]
+        public void DeveDevolverListaVaziaCasoNaoHajaLances()
+        {
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            var maiores = leiloeiro.TresMaiores;
+
+            Assert.AreEqual(0, maiores.Count);
+        }
+        public class FiltroDeLancesTest
+        {
+            [Test]
+            public void DeveSelecionarLancesEntre1000E3000()
+            {
+                Usuario joao = new Usuario("Joao");
+
+                FiltroDeLances filtro = new FiltroDeLances();
+                IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+            new Lance(joao,2000),
+            new Lance(joao,1000),
+            new Lance(joao,3000),
+            new Lance(joao, 800)});
+
+                Assert.AreEqual(1, resultado.Count);
+                Assert.AreEqual(2000, resultado[0].Valor, 0.00001);
+            }
+
+            [Test]
+            public void DeveSelecionarLancesEntre500E700()
+            {
+                Usuario joao = new Usuario("Joao");
+
+                FiltroDeLances filtro = new FiltroDeLances();
+                IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+            new Lance(joao,600),
+            new Lance(joao,500),
+            new Lance(joao,700),
+            new Lance(joao, 800)});
+
+                Assert.AreEqual(1, resultado.Count);
+                Assert.AreEqual(600, resultado[0].Valor, 0.00001);
+            }
+            [Test]
+            public void DeveSelecionarLancesMaioresQue5000()
+            {
+                Usuario joao = new Usuario("Joao");
+
+                FiltroDeLances filtro = new FiltroDeLances();
+                IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+        new Lance(joao,10000),
+        new Lance(joao, 800)});
+
+                Assert.AreEqual(1, resultado.Count);
+                Assert.AreEqual(10000, resultado[0].Valor, 0.00001);
+            }
+
+            [Test]
+            public void DeveEliminarMenoresQue500()
+            {
+                Usuario joao = new Usuario("Joao");
+
+                FiltroDeLances filtro = new FiltroDeLances();
+                IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+        new Lance(joao,400),
+        new Lance(joao, 300)});
+
+                Assert.AreEqual(0, resultado.Count);
+            }
+
+            [Test]
+            public void DeveEliminarEntre3000E5000()
+            {
+                Usuario joao = new Usuario("Joao");
+
+                FiltroDeLances filtro = new FiltroDeLances();
+                IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+        new Lance(joao,4000),
+        new Lance(joao, 3500)});
+
+                Assert.AreEqual(0, resultado.Count);
+            }
+        }
 
         // Daqui pra baixo testes do desafio cap1
         [Test]
