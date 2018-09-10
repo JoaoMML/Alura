ccfe930efeb48232b1e1f4487a720e5849e8c343
Joao Marcos de Moraes Lima <joaomarcos_rgt@hotmail.com>
2018-09-10 13:10:34 -0300

    Manipulando o reflection

--- T2/Reflection/Reflection/Program.cs
+++ T2/Reflection/Reflection/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reflection.Infra;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
+            var prefixos = new string[] { "http://localhost:5340/" };
+            var webApplication = new WebApplication(prefixos);
+            webApplication.Iniciar();
         }
     }
 }
