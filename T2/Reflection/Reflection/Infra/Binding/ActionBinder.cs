6630b140c9c5270f14ba3f4c500dbb047a6d1bd3
Joao Marcos de Moraes Lima <joaomarcos_rgt@hotmail.com>
2018-09-11 13:00:49 -0300

    Reflection cap5 aula 04 em 6:40

--- T2/Reflection/Reflection/Infra/Binding/ActionBinder.cs
+++ T2/Reflection/Reflection/Infra/Binding/ActionBinder.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Reflection.Infra.Binding
 {
     public class ActionBinder
     {
-        public object ObterMethodInfo(object controller, string path)
+        public MethodInfo ObterMethodInfo(object controller, string path)
         {
 
             // /Cambio/Calculo?moedaOrigem=BRL&moedaDestino=USD&valor=10
@@ -32,6 +33,11 @@ namespace Reflection.Infra.Binding
 
                 var tuplasNomeValor = ObterArgumentosNomeValores(queryString);
 
+                var nomeArgumentos = tuplasNomeValor.Select(tupla => tupla.Nome).ToArray();
+
+                var methodInfo = ObterMethodInfoAPartirDeNomeEArgumentos(nomeAction, nomeArgumentos, controller);
+                return methodInfo;
+
             }
         }
 
@@ -48,7 +54,42 @@ namespace Reflection.Infra.Binding
 
             }
         }
+        private MethodInfo ObterMethodInfoAPartirDeNomeEArgumentos(string nomeAction, string[] argumentos, object controller)
+        {
+            var argumentosCount = argumentos.Length;
 
+            var bindingFlags =
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.DeclaredOnly;
 
+            var metodos = controller.GetType().GetMethods(bindingFlags);
+            var sobrecargas = metodos.Where(metodo => metodo.Name == nomeAction);
+
+            foreach (var sobrecarga in sobrecargas)
+            {
+                var parametros = sobrecarga.GetParameters();
+
+                if (argumentosCount != parametros.Length)
+                    continue;
+
+                var match =
+                    parametros.All(
+                        parametro =>
+                            argumentos.Contains(parametro.Name)
+                    );
+
+                if (match)
+                {
+                    return sobrecarga;
+                }
+
+                throw new ArgumentException($"A sobrecarga do método {nomeAction} não foi encontrada!");
+            }
+        }
     }
 }
+
+
+
