11d1ecf11b87e2ec4372f7fb161935c649f50835
v010 <v010@benner.com.br>
2018-09-11 17:35:48 -0300

    Iniciando a validacao com js

--- T3/JavaScript/js/principal.js
+++ T3/JavaScript/js/principal.js
@@ -19,6 +19,29 @@ var imc = peso/ (altura*altura);
 console.log (peso);
 console.log (altura);
 console.log (imc);
+
+
+// criando variaveis de controle
+var pesoEhValido = true;
+var alturaEhvalida=true;
+
+// Fazendo Validação
+
+if (peso <= 0 || peso >= 1000){
+    console.log("Peso inválido");
+    pesoEhValido=false;
+    Tdimc.textContent ="Peso inválido"; 
+}
+if (altura <= 0 || altura >= 300){
+    console.log("Altura inválida");
+    alturaEhvalida = false;
+    Tdimc.textContent="Altura inválida";
+}
+
+if(pesoEhValido && alturaEhValida) {
+   var imc = peso / ( altura * altura); 
+}
+
 // Busca pela caixa do Imc
 var Tdimc = paciente.querySelector(".info-imc");
 // Exibindo na tela o valor calculado
