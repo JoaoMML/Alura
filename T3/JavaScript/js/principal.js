9a0f6ccd0352e24be94c6020e41eb57d554d4035
v010 <v010@benner.com.br>
2018-09-12 15:43:35 -0300

    Finalizando o for e a visualização com estilo do problema

--- T3/JavaScript/js/principal.js
+++ T3/JavaScript/js/principal.js
@@ -3,37 +3,46 @@ titulo.textContent = "Aparecida Nutricionista";
 
 
 // Buncando o peso do primeiro paciente 
-var paciente = document.querySelector("#primeiro-paciente");
-var Tdpeso = paciente.querySelector(".info-peso");
-var peso = Tdpeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-// Buscando altura
+for (var i = 0; i < pacientes.length; i++) {
 
-var Tdaltura = paciente.querySelector(".info-altura");
-var altura = Tdaltura.textContent;
+    paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var Tdpeso = paciente.querySelector(".info-peso");
+    var peso = Tdpeso.textContent;
 
-// criando variaveis de controle
-var pesoEhValido = true;
-var alturaEhValida = true;
+    // Buscando altura
 
-// Fazendo Validação
+    var Tdaltura = paciente.querySelector(".info-altura");
+    var altura = Tdaltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    // criando variaveis de controle
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    // Fazendo Validação
 
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
-}
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido")
+    }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido")
+        
+    }
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
