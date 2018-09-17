5d98f70e1273a04d540b9117a2a679f1309d0445
v010 <v010@benner.com.br>
2018-09-17 16:23:20 -0300

    Removendo com animações

--- T3/JavaScript/js/removendo-pacientes.js
+++ T3/JavaScript/js/removendo-pacientes.js
@@ -1,10 +1,11 @@
-var pacientes = document.querySelectorAll(".paciente");
-
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(event){
-    var alvoDoEvento = event.target;
-    var paiDoAlvo = alvoDoEvento.parentNode;
-
-    paiDoAlvo.remove();
-})
+tabela.addEventListener("dblclick", function (event) {
+    // var alvoDoEvento = event.target;
+    // var paiDoAlvo = alvoDoEvento.parentNode;
+    // paiDoAlvo.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function () {
+        event.target.parentNode.remove();
+    }, 500);
+});
